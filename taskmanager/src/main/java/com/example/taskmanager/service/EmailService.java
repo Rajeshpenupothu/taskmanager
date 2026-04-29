084b935541eca6e5805b629d17b68076f32f5895
RajeshPenupothu <rajeshpenupothu418@gmail.com>
2026-04-29 19:50:46 +0530

    fix sender

--- taskmanager/src/main/java/com/example/taskmanager/service/EmailService.java
+++ taskmanager/src/main/java/com/example/taskmanager/service/EmailService.java
@@ -1,8 +1,13 @@
-package com.example.taskmanager.service;
+import com.sendgrid.SendGrid;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.Method;
 
-import com.sendgrid.*;
 import com.sendgrid.helpers.mail.Mail;
-import com.sendgrid.helpers.mail.objects.*;
+import com.sendgrid.helpers.mail.objects.Email;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.TrackingSettings;
+import com.sendgrid.helpers.mail.objects.ClickTracking;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
